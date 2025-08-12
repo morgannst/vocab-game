6acf1ae3596641df67785ec5ff092796f374b44c
morgannst <yingyot.morgan@gmail.com>
2025-08-12 10:08:48 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change <REPO> to your GitHub repository name before deploying to GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: '/<REPO>/', // e.g. '/vocab-shapes-game/'
+  base: '/vocab-game/',
 })
